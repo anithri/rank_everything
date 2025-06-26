113e3ee60168070fd7cfcf537b3ade48903bb0d0
Scott M Parrish <anithri@gmail.com>
2025-06-26 11:40:13 -0600

    tweaks to navbar, sessions/new view and authentication.rb

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -35,7 +35,8 @@ module Authentication
     end
 
     def after_authentication_url
-      session.delete(:return_to_after_authenticating) || root_url
+      # session.delete(:return_to_after_authenticating) || root_url
+      root_path
     end
 
     def start_new_session_for(user)
